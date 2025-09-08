03279801f14a965153e716489db984ca4fb31cfa
jstrattonsmith <49959196+jstrattonsmith@users.noreply.github.com>
2025-09-08 10:58:10 -0400

    Removing specific sha dependency.

--- flake.nix
+++ flake.nix
@@ -6,15 +6,16 @@
   inputs.opam-repository.flake = false;
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.coq-library-undecidability.url = "github:uds-psl/coq-library-undecidability/coq-8.20";
-  # inputs.coq-library-undecidability.flake = false;
+  inputs.opam-coq-archive.url = "github:rocq-prover/opam/";
+  inputs.opam-coq-archive.flake = false;
+
   outputs =
     {
       self,
       opam-nix,
       flake-utils,
       opam-repository,
-      # coq-library-undecidability
+      opam-coq-archive,
       nixpkgs,
     }:
     flake-utils.lib.eachDefaultSystem (system:
@@ -28,13 +29,6 @@
         };
       legacyPackages =
         let
-          # pkgs = nixpkgs.legacyPackages.${system};
-          opam-coq-archive = pkgs.fetchFromGitHub {
-            "owner" = "rocq-prover";
-            "repo" = "opam";
-            "rev" = "faf3b3d4d2a25f0b9a312e416d1a43c826e25ec7";
-            "hash" = "sha256-9+5jrUbNpuC7Ct6rWIRUmmwuH1NAPkJofQC3BK/3yGg=";
-          };
           inherit (opam-nix.lib.${system}) queryToScope;
           scope =
             queryToScope
