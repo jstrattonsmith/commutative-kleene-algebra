464e01c16b6c95ef4cdc24360b3527c1798c7d6f
jstrattonsmith <49959196+jstrattonsmith@users.noreply.github.com>
2025-09-08 09:58:39 -0400

    Initial version of flake without vscoq setup.

--- flake.nix
+++ flake.nix
@@ -1,105 +1,57 @@
+
 {
-  description = "Build an opam project not in the repo, using sane defaults";
+  description = "Build a rocq (coq) package";
   inputs.opam-nix.url = "github:tweag/opam-nix";
+  inputs.opam-repository.url = "github:ocaml/opam-repository";
+  inputs.opam-repository.flake = false;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.coq-library-undecidability.url = "github:uds-psl/coq-library-undecidability";
+  # inputs.coq-library-undecidability.url = "github:uds-psl/coq-library-undecidability/coq-8.20";
+  # inputs.coq-library-undecidability.flake = false;
   outputs =
     {
       self,
       opam-nix,
-      coq-library-undecidability,
       flake-utils,
+      opam-repository,
+      # coq-library-undecidability
+      nixpkgs,
     }:
     flake-utils.lib.eachDefaultSystem (system: {
+      # devShells.default = pkgs.mkShell {
+      #     packages = with self.legacyPackages.${system}; [
+      #       vscoq-language-server
+      #       coq-library-undecidability
+      #       coq
+      #     ];
+      #   };
       legacyPackages =
         let
-          inherit (opam-nix.lib.${system}) buildOpamProject;
-          scope = buildOpamProject { } "coq-library-undecidability" coq-library-undecidability {
-            ocaml-system = "*";
+          pkgs = nixpkgs.legacyPackages.${system};
+          opam-coq-archive = pkgs.fetchFromGitHub {
+            "owner" = "rocq-prover";
+            "repo" = "opam";
+            "rev" = "faf3b3d4d2a25f0b9a312e416d1a43c826e25ec7";
+            "hash" = "sha256-9+5jrUbNpuC7Ct6rWIRUmmwuH1NAPkJofQC3BK/3yGg=";
           };
+          inherit (opam-nix.lib.${system}) queryToScope;
+          scope =
+            queryToScope
+              {
+                repos = [
+                  "${opam-repository}"
+                  "${opam-coq-archive}/released"
+                ];
+              }
+              {
+                # rocq-prover = "8.20";
+                coq-library-undecidability = "*"; # "1.1.2+8.20";
+                ocaml-base-compiler = "*"; # "4.14.1+flambda";
+                vscoq-language-server = "*";
+              };
         in
         scope;
 
-      packages.default = self.legacyPackages.${system}.coq-library-undecidability;
+        packages.default = self.legacyPackages.${system}.coq-library-undecidability;
     });
 }
-# {
-#   description = "Build a rocq (coq) package";
-#   inputs.opam-nix.url = "github:tweag/opam-nix";
-#   inputs.opam-repository.url = "github:ocaml/opam-repository";
-#   inputs.opam-repository.flake = false;
-#   inputs.nixpkgs.url = "github:nixos/nixpkgs";
-#   inputs.flake-utils.url = "github:numtide/flake-utils";
-#   outputs =
-#     {
-#       self,
-#       opam-nix,
-#       flake-utils,
-#       opam-repository,
-#       nixpkgs,
-#     }:
-#     flake-utils.lib.eachDefaultSystem (system: {
-#       legacyPackages =
-#         let
-#           pkgs = nixpkgs.legacyPackages.${system};
-#           opam-coq-archive = pkgs.fetchFromGitHub {
-#             "owner" = "uds-psl";
-#             "repo" = "coq-library-undecidability";
-#             "rev" = "70dfc56f33a6e4835281044e68aa68279989047e";
-#             "hash" = "sha256-yGXw7khdHcro1yTQrr2yngtOAVy8jHJEO7G93Znp5Cc=";
-#           };
-#           inherit (opam-nix.lib.${system}) queryToScope;
-#           scope =
-#             queryToScope
-#               {
-#                 repos = [
-#                   "${opam-repository}"
-#                   "${opam-coq-archive}/extra-dev"
-#                 ];
-#               }
-#               {
-#                 rocq-prover = "*";
-#                 coq-inf-seq-ext = "*";
-#                 ocaml-base-compiler = "*";
-#               };
-#         in
-#         scope;
-
-#       packages.default = self.legacyPackages.${system}.coq-inf-seq-ext;
-#     });
-# }
-
-# {
-#   inputs = {
-#     opam-nix.url = "github:tweag/opam-nix";
-#     flake-utils.url = "github:numtide/flake-utils";
-#     nixpkgs.follows = "opam-nix/nixpkgs";
-#   };
-#   outputs =
-#     {
-#       self,
-#       flake-utils,
-#       opam-nix,
-#       nixpkgs,
-#     }@inputs:
-#     # Don't forget to put the package name instead of `throw':
-#     let
-#       package = throw "Put the package name here!";
-#     in
-#     flake-utils.lib.eachDefaultSystem (
-#       system:
-#       let
-#         pkgs = nixpkgs.legacyPackages.${system};
-#         on = opam-nix.lib.${system};
-#         scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
-#         overlay = final: prev: {
-#           # Your overrides go here
-#         };
-#       in
-#       {
-#         legacyPackages = scope.overrideScope overlay;
-
-#         packages.default = self.legacyPackages.${system}.${package};
-#       }
-#     );
-# }
